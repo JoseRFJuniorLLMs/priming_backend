8fb6433313860881815f3aa8ca555ea53697d7ff
priming <web2ajax@gmail.com>
2023-11-17 01:02:29 +0000

    first commit

---
+++ src/main/java/com/infybuzz/entity/PhaseStudent.java
@@ -0,0 +1,24 @@
+package com.infybuzz.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+
+import java.util.List;
+
+@Node("phasesstudent")
+@EntityScan("com.infybuzz.entity")
+public class PhaseStudent {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Property
+    private Long pk_student;
+    @Property
+    private List phasestudent;
+
+    public PhaseStudent() {
+    }
+}
---
+++ src/main/java/com/infybuzz/entity/TextStudant.java
@@ -0,0 +1,20 @@
+package com.infybuzz.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.w3c.dom.Text;
+
+@Node("textstudant")
+@EntityScan("com.infybuzz.entity")
+public class TextStudant {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Property
+    private Long pk_student;
+    @Property
+    private Text textstuant;
+}
