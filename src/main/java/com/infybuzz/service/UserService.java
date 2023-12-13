ab117d7fac275286252cdd95b962fe1a25bfb149
Fritz Henrique <fritzhenrique.dev@gmail.com>
2023-12-12 23:25:42 -0400

    feat[User Service]: create services to get user and create user

--- src/main/java/com/infybuzz/service/UserService.java
+++ src/main/java/com/infybuzz/service/UserService.java
@@ -1,7 +1,9 @@
 package com.infybuzz.service;
 
+import com.infybuzz.DTO.UserDTO;
 import com.infybuzz.entity.User;
 import com.infybuzz.repository.UserRepository;
+import com.infybuzz.transformer.UserTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,18 @@ import java.util.List;
 
 @Service
 public class UserService {
-
     @Autowired
     UserRepository userRepository;
 
-    public List<User> getStudentByNameAndPassword(String name, String password) {
-        return userRepository.getByNameAndPassword(name, password);
+    private final UserTransformer userTransformer = new UserTransformer();
+
+    public UserDTO getUserById(Integer id) {
+        User user = userRepository.findById(Long.valueOf(id)).get();
+        return userTransformer.toDto(user);
+    }
+
+    public User saveUser(User user) {
+        user = userRepository.createUser(user.getName(), user.getPassword(), user.getEmail(), user.getPhone(), user.getCpf());
+        return user;
     }
 }
