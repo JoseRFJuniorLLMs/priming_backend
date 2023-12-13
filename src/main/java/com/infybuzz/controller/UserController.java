fe99ac9078035c43c84295edce4afe88aa33e894
Fritz Henrique <fritzhenrique.dev@gmail.com>
2023-12-12 23:23:25 -0400

    feat[Student Service]: created endpoint to create a new user

--- src/main/java/com/infybuzz/controller/UserController.java
+++ src/main/java/com/infybuzz/controller/UserController.java
@@ -4,10 +4,9 @@ import com.infybuzz.DTO.UserDTO;
 import com.infybuzz.entity.User;
 import com.infybuzz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,8 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-
-
+    @PostMapping("")
+    public ResponseEntity create(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(user));
+    }
 }
