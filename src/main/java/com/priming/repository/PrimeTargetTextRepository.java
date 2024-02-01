5c5f604fdee1d2b5aed8868741e2679e4a0adc05
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-01 18:46:18 +0000

    update

---
+++ src/main/java/com/priming/repository/PrimeTargetTextRepository.java
@@ -0,0 +1,14 @@
+package com.priming.repository;
+
+import com.priming.entity.PrimeTargetTextCollection;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PrimeTargetTextRepository extends MongoRepository <PrimeTargetTextCollection, String> {
+    List<PrimeTargetTextCollection> findByPrimeAndTargetText(String prime, String target);
+
+    List<PrimeTargetTextCollection> findByTarget(String target);
+}
---
+++ src/main/java/com/priming/service/PrimeTargetTextService.java
@@ -0,0 +1,24 @@
+package com.priming.service;
+
+import com.priming.entity.PrimeTargetCollection;
+import com.priming.entity.PrimeTargetTextCollection;
+import com.priming.repository.PrimeTargetTextRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PrimeTargetTextService {
+
+    @Autowired
+    private PrimeTargetTextRepository primeTargetTextRepository;
+
+    public List<PrimeTargetTextCollection> findByPrimeAndTarget(String prime, String target) {
+        return primeTargetTextRepository.findByPrimeAndTargetText(prime, target);
+    }
+
+    public List<PrimeTargetTextCollection> findByTarget(String target) {
+        return primeTargetTextRepository.findByTarget(target);
+    }
+}
